281756dbbc83ea2f7b0ecb815c0d9dbd77710344
c-warnick <charlie.warnick@gmail.com>
2017-01-03 12:34:48 -0500

    removing unnecessary content

--- src/helpers/authentication.js
+++
@@ -1,9 +0,0 @@
-
-module.exports.auth = function(req, res, next) {
-  console.log(`${req.session} : ${req.session.authenticated}`)
-  if (req.session && req.session.authenticated)
-    return next();
-  else
-    req.session.requestedPath = req.originalUrl;
-    res.redirect('/login');
-};
